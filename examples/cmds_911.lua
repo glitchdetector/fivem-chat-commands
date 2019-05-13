babceda1c49c598136a66d676571f4bc02cd8869
Vegard Bendiktsen <glitchdetector@gmail.com>
2019-05-14 00:42:57 +0200

    911 example result based on online dispatch

--- examples/cmds_911.lua
+++ examples/cmds_911.lua
@@ -11,19 +11,23 @@ CommandPack("Information Commands", "glitchdetector", {
         cb = function(source, message, command, _)
             if SETTINGS.use_vrp then
                 local users = vRP.getUsersByPermission({"police.pc"})
+                local alerted = 0
                 for _, user in next, users do
+                    alerted = alerted + 1
                     TriggerClientEvent("chat:addMessage", user, {args = {command.title, message}, color = command.color})
                 end
-                TriggerClientEvent("chat:addMessage", source, {args = {command.title, message}, color = command.color})
+                TriggerClientEvent("chat:addMessage", source, {args = {command.title, (alerted > 0 and "Your message has been sent" or "No available dispatch")}, color = command.color})
             elseif SETTINGS.use_esx then
 	            local xPlayers = ESX.GetPlayers()
+                local alerted = 0
                 for i=1, #xPlayers, 1 do
              		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
              		if xPlayer.job.name == 'police' or xPlayer.job.name == 'ambulance' then
+                        alerted = alerted + 1
             			TriggerClientEvent("chat:addMessage", xPlayers[i], {args = {command.title, message}, color = command.color})
                     end
                 end
-                TriggerClientEvent("chat:addMessage", source, {args = {command.title, message}, color = command.color})
+                TriggerClientEvent("chat:addMessage", source, {args = {command.title, (alerted > 0 and "Your message has been sent" or "No available dispatch")}, color = command.color})
             else
                 TriggerClientEvent("chat:addMessage", -1, {args = {command.title, message}, color = command.color})
             end
